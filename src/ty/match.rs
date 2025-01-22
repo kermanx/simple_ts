052ed7bd179c75e832c23169aadd2ae74b28c581
_Kerman <kermanx@qq.com>
2025-01-22 16:14:26 +0800

    wip: match callable types

--- src/ty/match.rs
+++ src/ty/match.rs
@@ -1,15 +1,17 @@
 use oxc::semantic::SymbolId;
 use rustc_hash::FxHashMap;
 
-use super::{intersection::IntersectionTypeBuilder, unresolved::UnresolvedType, Ty};
+use super::{
+  callable::CallableType, intersection::IntersectionTypeBuilder, unresolved::UnresolvedType, Ty,
+};
 use crate::Analyzer;
 
 pub enum MatchResult<'a> {
+  Error,
   Unmatched,
   Matched,
   Inferred(FxHashMap<SymbolId, Ty<'a>>),
   Multiple(Vec<MatchResult<'a>>),
-  Error,
 }
 
 impl<'a> Analyzer<'a> {
@@ -21,9 +23,10 @@ impl<'a> Analyzer<'a> {
     match (target, pattern) {
       (target, pattern) if target == pattern => MatchResult::Matched,
 
-      (target, Ty::Unresolved(UnresolvedType::InferType(s))) => MatchResult::Inferred({
+      (matched, Ty::Unresolved(UnresolvedType::InferType(s)))
+      | (Ty::Unresolved(UnresolvedType::InferType(s)), matched) => MatchResult::Inferred({
         let mut map = FxHashMap::default();
-        map.insert(s, target);
+        map.insert(s, matched);
         map
       }),
 
@@ -41,8 +44,10 @@ impl<'a> Analyzer<'a> {
       (Ty::UniqueSymbol(_), Ty::Symbol) => MatchResult::Matched,
 
       (Ty::Record(target), Ty::Record(pattern)) => todo!(),
-      (Ty::Function(target), Ty::Function(pattern)) => todo!(),
-      (Ty::Constructor(target), Ty::Constructor(pattern)) => todo!(),
+      (Ty::Function(target), Ty::Function(pattern)) => self.match_callable_types(target, pattern),
+      (Ty::Constructor(target), Ty::Constructor(pattern)) => {
+        self.match_callable_types(target, pattern)
+      }
       (Ty::Namespace(target), Ty::Namespace(pattern)) => todo!(),
 
       (Ty::Union(target), Ty::Union(pattern)) => {
@@ -99,6 +104,89 @@ impl<'a> Analyzer<'a> {
       (_, Ty::Generic(_) | Ty::Intrinsic(_)) => MatchResult::Error,
 
       _ => MatchResult::Unmatched,
+    }
+  }
+
+  fn match_callable_types<const CTOR: bool>(
+    &mut self,
+    target: &'a CallableType<'a, CTOR>,
+    pattern: &'a CallableType<'a, CTOR>,
+  ) -> MatchResult<'a> {
+    // Step1: Match type parameters
+    if target.type_params.len() != pattern.type_params.len() {
+      return MatchResult::Unmatched;
+    }
+    for (target, pattern) in target.type_params.iter().zip(pattern.type_params.iter()) {
+      if target.constraint == pattern.constraint {
+        continue;
+      }
+      // Note: Contravariance - `pattern.constraint extends target.constraint`
+      match self.match_types(
+        pattern.constraint.unwrap_or(Ty::Unknown),
+        target.constraint.unwrap_or(Ty::Unknown),
+      ) {
+        MatchResult::Error => return MatchResult::Error,
+        MatchResult::Unmatched => return MatchResult::Unmatched,
+        MatchResult::Matched => {}
+        MatchResult::Inferred(_) => {
+          // Somehow in TypeScript this is ignored
+        }
+        MatchResult::Multiple(_) => unreachable!("Unmatch or single match"),
+      }
+    }
+
+    let mut inferred = FxHashMap::default();
+
+    // Step2: Match this type
+    if let (Some(target), Some(pattern)) = (target.this_param, pattern.this_param) {
+      match self.match_types(pattern, target) {
+        MatchResult::Error => return MatchResult::Error,
+        MatchResult::Unmatched => return MatchResult::Unmatched,
+        MatchResult::Matched => {}
+        MatchResult::Inferred(map) => {
+          inferred.extend(map);
+        }
+        MatchResult::Multiple(_) => unreachable!("Unmatch or single match"),
+      }
+    }
+
+    // Step3: Match parameters
+    for (index, (target_optional, mut target)) in target.params.iter().enumerate() {
+      if let Some((pattern_optional, mut pattern)) = pattern.params.get(index) {
+        if target_optional != pattern_optional {
+          target = self.get_optional_type(*target_optional, target);
+          pattern = self.get_optional_type(*pattern_optional, pattern);
+        }
+        match self.match_types(pattern, target) {
+          MatchResult::Error => return MatchResult::Error,
+          MatchResult::Unmatched => return MatchResult::Unmatched,
+          MatchResult::Matched => {}
+          MatchResult::Inferred(map) => {
+            inferred.extend(map);
+          }
+          MatchResult::Multiple(_) => unreachable!("Unmatch or single match"),
+        }
+      } else if *target_optional {
+        // Optional parameter, matched
+      } else {
+        // TODO: Check rest parameter
+        return MatchResult::Unmatched;
+      }
+    }
+
+    // Step4: Match rest parameter
+    // TODO: Check rest parameter
+
+    // Step5: Match return type
+    match self.match_types(target.return_type, pattern.return_type) {
+      MatchResult::Error => MatchResult::Error,
+      MatchResult::Unmatched => MatchResult::Unmatched,
+      MatchResult::Matched => MatchResult::Matched,
+      MatchResult::Inferred(map) => {
+        inferred.extend(map);
+        MatchResult::Inferred(inferred)
+      }
+      MatchResult::Multiple(_) => unreachable!("Unmatch or single match"),
     }
   }
 }
